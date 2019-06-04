a3b327bf81e20393a239f71f3de9649f59e0834e
limadebarros <limadebarros84@gmail.com>
2019-06-04 17:49:19 +0200

    Style

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,38 @@
 import React from 'react'
+import tank from '../images/tank2.png'
+
+import './LoginForm.css'
 
 export default function LoginForm(props) {
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={props.onSubmit}>
-        <label>
-        Username: &nbsp;
-        <input 
-          type="text"
-          name="userName" 
-          value={props.values.user_name}
-          onChange={props.onChange} />
-        </label>
-        <br/>
-        <label>
-        Password: &nbsp;
-        <input 
-          type="password" 
-          name="password" 
-          value={props.values.password} 
-          onChange={props.onChange} />
-        </label>
-        
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <main>
+      <div className="image">
+        <img src={tank} alt="tank"/>
+      </div>
+      <div className="login-form">
+        <h2>Login</h2>
+        <form onSubmit={props.onSubmit}>
+          <label>
+          Username: &nbsp;
+          <input 
+            type="text"
+            name="userName" 
+            value={props.values.user_name}
+            onChange={props.onChange} />
+          </label>
+          <br/>
+          <label>
+          Password: &nbsp;
+          <input 
+            type="password" 
+            name="password" 
+            value={props.values.password} 
+            onChange={props.onChange} />
+          </label>
+          
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </main>
   )
 }
