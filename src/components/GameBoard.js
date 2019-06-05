bd43741fbe5f216c446c3b375c4eb0a379a40525
rafaelrolivares <rafaelrolivares@gmail.com>
2019-06-05 15:12:33 +0200

    game board player one

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,17 +1,30 @@
 import * as React from 'react'
 
 
-export default function GameDetails(props) {
+export default function GameBoard(props) {
 
   const { game } = props
 
   if (!game) return 'Loading...'
   console.log(game.game)
 
+  const gameBoardP1 = (<div><p>{game.game.playerOne}</p>
+  <li>{game.game.board[0]}</li>
+  <li>{game.game.board[1]}</li>
+  <li>{game.game.board[2]}</li>
+  <li>{game.game.board[3]}</li>
+  <li>{game.game.board[4]}</li>
+  <li>{game.game.board[5]}</li>
+  <li>{game.game.board[6]}</li>
+  <li>{game.game.board[7]}</li>
+  <li>{game.game.board[8]}</li>
+  <li>{game.game.board[9]}</li>
+  </div>)
+
+
   return (<div>
     <h1>Welcome to Game # {game.game.id}</h1>
-    <i>{game.game.board}</i>
-    <p>Player One (id): {game.game.playerOne}</p>
-    <p>Player Two (id): {game.game.playerTwo}</p>
+    <i>{gameBoardP1}</i>
+    <p>Player One (id): {game.game.playerOne} - Player Two (id): {game.game.playerTwo}</p>
   </div>)
 }
