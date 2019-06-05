9699cc065ea6e24fff95bf5889c2e73045ee7c9c
limadebarros <limadebarros84@gmail.com>
2019-06-05 09:26:08 +0200

    Styled and sound off for now

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import LoginForm from './LoginForm'
 import {connect} from 'react-redux'
 import {login} from '../actions/auth'
-import bgsound from '../sounds/bgsound.mp3'
-import Sound from 'react-sound'
+// import bgsound from '../sounds/bgsound.mp3'
+// import Sound from 'react-sound'
 
 class LoginFormContainer extends React.Component {
   state = { 
@@ -29,18 +29,18 @@ class LoginFormContainer extends React.Component {
       onSubmit={this.onSubmit} 
       onChange={this.onChange} 
       values={this.state} />
-
-      <Sound
-        url={bgsound}
-        playStatus={Sound.status.PLAYING}
-        playFromPosition={100 /* in milliseconds */}
-        onLoading={this.handleSongLoading}
-        onPlaying={this.handleSongPlaying}
-        loop={true}
-      />
      </div>
     )
   }
 }
 
 export default connect(null, {login})(LoginFormContainer)
+
+/* <Sound
+url={bgsound}
+playStatus={Sound.status.PLAYING}
+playFromPosition={100 /* in milliseconds */
+// onLoading={this.handleSongLoading}
+// onPlaying={this.handleSongPlaying}
+// loop={true}
+// /> */}
